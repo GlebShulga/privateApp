d74763f430464196da638f68ccb2d64b266e2f36
GlebShulga <Shulga_Gleb@hotmail.com>
2023-05-19 11:27:17 +0400

    update next config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,14 @@
 /** @type {import('next').NextConfig} */
 
+const repo = 'privateApp'
+const assetPrefix = `/${repo}/`
+const basePath = `/${repo}`
+
 
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: assetPrefix,
+  basePath: basePath,
   images: {
     loader: 'imgix',
     path: 'https://private-9498.imgix.net',
