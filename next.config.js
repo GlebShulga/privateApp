b1554eb6d7548abc2db0c769eb87a0d084d6ebd6
GlebShulga <Shulga_Gleb@hotmail.com>
2023-05-19 11:58:02 +0400

    update src for socialLinks

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 
-const repo = 'privateApp'
-const assetPrefix = `/${repo}/`
-const basePath = `/${repo}`
-
-
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: assetPrefix,
-  basePath: basePath,
   images: {
     loader: 'imgix',
     path: 'https://private-9498.imgix.net',
--- src/constants/socialLinks.ts
+++ src/constants/socialLinks.ts
@@ -2,16 +2,16 @@ export const socialLinks = [
   {
     name: "github",
     url: "https://github.com/GlebShulga",
-    iconSrc: "./assets/icon-github.svg",
+    iconSrc: "/privateApp/assets/icon-github.svg",
   },
   {
     name: "linkedin",
     url: "https://www.linkedin.com/in/gleb-shulga/",
-    iconSrc: "./assets/icon-linkedin.svg",
+    iconSrc: "/privateApp/assets/icon-linkedin.svg",
   },
   {
     name: "email",
     url: "mailto:shulga_gleb@hotmail.com?subject=Hello There!&amp;body=Hi Gleb, so I was looking at your website and...",
-    iconSrc: "./assets/icon-email.svg",
+    iconSrc: "/privateApp/assets/icon-email.svg",
   },
 ];
