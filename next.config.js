fac55c234bdf745f0d295dabe311e80c8cd81b08
GlebShulga <Shulga_Gleb@hotmail.com>
2023-05-20 07:34:30 +0400

    update next.config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-const repo = isGithubActions ? process.env.GITHUB_REPOSITORY.replace(/.*?\//, '') : '';
+const isGithubActions = process.env.ACTIONS || false;
+const repo = isGithubActions ? process.env.REPOSITORY.replace(/.*?\//, '') : '';
 
 const assetPrefix = isGithubActions ? `/${repo}/` : '';
 const basePath = isGithubActions ? `/${repo}` : '';
