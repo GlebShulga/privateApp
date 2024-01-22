3706b7057aec4b2720facc92e4d78540f461e871
GlebShulga <Shulga_Gleb@hotmail.com>
2024-01-22 11:18:58 +0400

    update next.config

--- next.config.js
+++ next.config.js
@@ -1,19 +1,20 @@
 /** @type {import('next').NextConfig} */
 
 const isGithubActions = process.env.ACTIONS || false;
-const repo = isGithubActions ? process.env.REPOSITORY.replace(/.*?\//, '') : '';
+const repo = isGithubActions ? process.env.REPOSITORY.replace(/.*?\//, "") : "";
 
-const assetPrefix = isGithubActions ? `/${repo}/` : '';
-const basePath = isGithubActions ? `/${repo}` : '';
+const assetPrefix = isGithubActions ? `/${repo}/` : "";
+const basePath = isGithubActions ? `/${repo}` : "";
 
 const nextConfig = {
+  output: "export",
   reactStrictMode: true,
   assetPrefix: assetPrefix,
   basePath: basePath,
   images: {
-    loader: 'imgix',
-    path: 'https://private-9498.imgix.net',
+    loader: "imgix",
+    path: "https://private-9498.imgix.net",
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
