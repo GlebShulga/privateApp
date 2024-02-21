d0ee242d4e3f5eae131633748092f3feaf6b8c4c
GlebShulga <Shulga_Gleb@hotmail.com>
2024-02-21 20:23:44 +0400

    fixed calculateYearsAndMonths function

--- src/helpers/calculateYearsAndMonths.ts
+++ src/helpers/calculateYearsAndMonths.ts
@@ -1,15 +1,16 @@
 export const calculateYearsAndMonths = (
-    date: Date
-  ): { years: number; months: number } => {
-    const currentDate = new Date();
-    let years = currentDate.getFullYear() - date.getFullYear();
-    let months = currentDate.getMonth() - date.getMonth();
-    const monthsInYear = 12;
+  date: Date
+): { years: number; months: number } => {
+  const currentDate = new Date();
+  const monthIndexAdjustment = 1;
+  let years = currentDate.getFullYear() - date.getFullYear();
+  let months = currentDate.getMonth() - date.getMonth() + monthIndexAdjustment;
+  const monthsInYear = 12;
 
-    if (months < 0) {
-      years--;
-      months += monthsInYear;
-    }
+  if (months < 0) {
+    years--;
+    months += monthsInYear;
+  }
 
-    return { years, months };
-  };
+  return { years, months };
+};
